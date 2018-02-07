7f4296bccb60317f5193978cac646f88f262a43f
M-atilda <lmatilda.gp100@gmail.com>
2018-02-07 22:04:34 +0900

    modify that pressure gradient's treatment of obstacle's surface is treated in pressure calculator

--- lib/MAC/func.ex
+++ lib/MAC/func.ex
@@ -92,11 +92,8 @@ defmodule MAC.Func do
     dx, dy,
     x_size, y_size,
     omega do
-    IO.puts "[Debug] pressure   #{inspect pressure}"
 
     divide_val = 2*(1/(dx*dx) + 1/(dy*dy))
-
-    IO.puts "[Debug] derive_val has calced."
 
     # divide to 4 spaces for pararell processing
     x_half_size = round((x_size-1) / 2)
@@ -109,33 +106,17 @@ defmodule MAC.Func do
     {left_down, left_down_dp} = Task.await(left_down_dp_task)
     {right_up, right_up_dp} = Task.await(right_up_dp_task)
     {right_down, right_down_dp} = Task.await(right_down_dp_task)
-    IO.puts "[Debug] derivePrePartially has finished."
-    try do
-      left_up_res = Task.async(fn ->
-        flatten_list = List.flatten(left_up_dp)
-        IO.puts "flatten_list #{inspect flatten_list}"
-        pow_res_list = Enum.map flatten_list, (&(&1*&1))
-        IO.puts "pow_res_list #{inspect pow_res_list}"
-        res = :lists.sum pow_res_list
-        IO.puts "res #{inspect res}"
-        res end)
-      left_down_res = Task.async(fn -> List.flatten(left_down_dp) |> Enum.map(&(&1*&1)) |> :lists.sum end)
-      right_up_res = Task.async(fn -> List.flatten(right_up_dp) |> Enum.map(&(&1*&1)) |> :lists.sum end)
-      right_down_res = Task.async(fn -> List.flatten(right_down_dp) |> Enum.map(&(&1*&1)) |> :lists.sum end)
-      up_side = Enum.map(:lists.zip(left_up, right_up), fn({l, r}) -> List.to_tuple(l ++ r) end)
-      down_side = Enum.map(:lists.zip(left_down, right_down), fn({l, r}) -> List.to_tuple(l ++ r) end)
-      up_side_dp = Enum.map(:lists.zip(left_up_dp, right_up_dp), fn({l, r}) -> List.to_tuple(l ++ r) end)
-      down_side_dp = Enum.map(:lists.zip(left_down_dp, right_down_dp), fn({l, r}) -> List.to_tuple(l ++ r) end)
-      IO.puts "[Debug] residual has calced."
-      {(up_side ++ down_side) |> List.to_tuple,
-      (up_side_dp ++ down_side_dp) |> List.to_tuple,
-       Task.await(left_up_res) + Task.await(left_down_res) + Task.await(right_up_res) + Task.await(right_down_res)}
-    rescue
-      e ->
-        IO.puts "[Error] #{inspect e} <derivePreStep>"
-        IO.puts "pressure #{inspect pressure}"
-        raise e
-    end
+    left_up_res = Task.async(fn -> List.flatten(left_up_dp) |> Enum.map(&(&1*&1)) |> :lists.sum end)
+    left_down_res = Task.async(fn -> List.flatten(left_down_dp) |> Enum.map(&(&1*&1)) |> :lists.sum end)
+    right_up_res = Task.async(fn -> List.flatten(right_up_dp) |> Enum.map(&(&1*&1)) |> :lists.sum end)
+    right_down_res = Task.async(fn -> List.flatten(right_down_dp) |> Enum.map(&(&1*&1)) |> :lists.sum end)
+    up_side = Enum.map(:lists.zip(left_up, right_up), fn({l, r}) -> List.to_tuple(l ++ r) end)
+    down_side = Enum.map(:lists.zip(left_down, right_down), fn({l, r}) -> List.to_tuple(l ++ r) end)
+    up_side_dp = Enum.map(:lists.zip(left_up_dp, right_up_dp), fn({l, r}) -> List.to_tuple(l ++ r) end)
+    down_side_dp = Enum.map(:lists.zip(left_down_dp, right_down_dp), fn({l, r}) -> List.to_tuple(l ++ r) end)
+    {(up_side ++ down_side) |> List.to_tuple,
+    (up_side_dp ++ down_side_dp) |> List.to_tuple,
+     Task.await(left_up_res) + Task.await(left_down_res) + Task.await(right_up_res) + Task.await(right_down_res)}
   end
   defp deriveDPPartially pressure, right_side, bc_field, divide_val, omega, {dx,dy}, {x_size,y_size}, {x_range,y_range} do
     for j <- y_range do
@@ -153,9 +134,46 @@ defmodule MAC.Func do
             (((id(pressure, {max_i,j}) + id(pressure, {min_i,j})) / x_width) + ((id(pressure, {i,max_j}) + id(pressure, {i,min_j})) / y_width) - id(right_side, {i,j})) / divide_val - id(pressure, {i,j})
           end
         else
-          0
+          0.0
         end
-        {id(pressure, {i,j}) + omega * dp, dp}
+        if !id(bc_field, {i,j}) do
+          {id(pressure, {i,j}) + omega * dp, dp}
+        else
+          if id(bc_field, {i,j}) == "null" do
+            cond do
+              id(bc_field, {i-1,j}) != "null" ->
+                cond do
+                  id(bc_field, {i,j+1}) != "null" ->
+                    #top left
+                    {id(pressure, {i-1,j+1}), 0.0}
+                  id(bc_field, {i,j-1}) != "null" ->
+                    #bottom left
+                    {id(pressure, {i-1,j-1}), 0.0}
+                  true ->
+                    {id(pressure, {i-1,j}), 0.0}
+                end
+              id(bc_field, {i+1,j}) != "null" ->
+                cond do
+                  id(bc_field, {i,j+1}) != "null" ->
+                    #top right
+                    {id(pressure, {i+1,j+1}), 0.0}
+                  id(bc_field, {i,j-1}) != "null" ->
+                    #top left
+                    {id(pressure, {i+1,j-1}), 0.0}
+                  true ->
+                    {id(pressure, {i+1,j}), 0.0}
+                end
+              id(bc_field, {i,j-1}) != "null" ->
+                {id(pressure, {i,j-1}), 0.0}
+              id(bc_field, {i,j+1}) != "null" ->
+                {id(pressure, {i,j+1}), 0.0}
+              true ->
+                {0.0, 0.0}
+            end
+          else
+            {id(bc_field, {i,j}), dp}
+          end
+        end
       end
     end
     |> splitField([], [])
